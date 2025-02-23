1985c2e1f0a388ae3b15574fdb312314a87cfe39
Angel Genev <angel.genev@volley-labs.com>
2025-02-24 01:04:15 +0200

    Inginer

---
+++ Snowpipe-Connection-manager-UI/SnowpipeCM-UI.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.SqlServer.Dts.Runtime;
+using Microsoft.SqlServer.Dts.Runtime.Design;
+using System;
+
+namespace Snowpipe_Connection_manager_UI
+{
+    public class SnowpipeCM_UI : IDtsConnectionManagerUI
+    {
+        public void Delete(System.Windows.Forms.IWin32Window parentWindow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(System.Windows.Forms.IWin32Window parentWindow, Connections connections, ConnectionManagerUIArgs connectionUIArg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(ConnectionManager connectionManager, IServiceProvider serviceProvider)
+        {
+            var test = new AboutBox1();
+            test.Show();
+        }
+
+        public bool New(System.Windows.Forms.IWin32Window parentWindow, Connections connections, ConnectionManagerUIArgs connectionUIArg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Snowpipe-Connection-manager/SnowpipeCM.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.SqlServer.Dts.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snowpipe_Connection_manager
+{
+    [DtsConnection(
+    DisplayName = "SNOWPIPE",
+    ConnectionType = "SNOWPIPE",
+    Description = "Connection Manager for Snowpipe",
+    UITypeName = "Snowpipe_Connection_manager_UI.SnowpipeCM_UI," +
+        "Snowpipe-Connection-manager-UI, Version=1.0.0.0, Culture=neutral, PublicKeyToken=24206b76cd1fde6c, processorArchitecture=MSIL")]
+    public class SnowpipeCM : ConnectionManagerBase
+    {
+        public override object AcquireConnection(object txn)
+        {
+            return base.AcquireConnection(txn);
+        }
+
+        public override void ReleaseConnection(object txn) { base.ReleaseConnection(txn); }
+
+        public override void Update(ref string ObjectXml)
+        {
+            base.Update(ref ObjectXml);
+        }
+
+        public override DTSExecResult Validate(IDTSInfoEvents infoEvents)
+        {
+            return base.Validate(infoEvents);
+        }
+    }
+}
